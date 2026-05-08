ae2631a30f772968159b7f48d88fa8f33cd696cc
crayzepanda <195377207+crayzepanda@users.noreply.github.com>
2026-05-09 01:49:30 +0200

    mise a jour

--- test.py
+++ test.py
@@ -3,4 +3,8 @@ import random
 def number_aleatoire():
     return random.randint(1, 100)
 
+def decimal_aleatoire():
+    return random.uniform(0, 1)
+
 print("Le nombre aléatoire généré est :", number_aleatoire())
+print("Le nombre décimal aléatoire généré est :", decimal_aleatoire())
